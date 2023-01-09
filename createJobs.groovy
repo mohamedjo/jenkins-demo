cad89eb18d9a0c5fe9db0d09a32fd2b9561ce648
mohamedjo <mohamedyuossef979@gmail.com>
2023-01-09 14:45:41 +0200

    update project url

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/mohamedjo/CocusPortugalBackendTask.git'
+                        url 'https://github.com/mohamedjo/backendTaskCocusPortugal.git'
                     }
                     branch 'master'
                 }
